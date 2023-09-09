d07f352eb4308382785bed8747d248a683da3bf3
VasyanPupkin228 <tgoroshek@gmail.com>
2023-09-09 14:42:34 +0300

    HW done

--- src/main/java/ru/hogwarts/school/service/AvatarService.java
+++ src/main/java/ru/hogwarts/school/service/AvatarService.java
@@ -1,5 +1,7 @@
 package ru.hogwarts.school.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -21,17 +23,23 @@ import static java.nio.file.StandardOpenOption.CREATE_NEW;
 
 @Service
 public class AvatarService {
-    StudentService studentService;
-    AvatarRepo avatarRepo;
+    private StudentService studentService;
+    private AvatarRepo avatarRepo;
+    private Logger logger;
+    private String currMethod;
     @Value("${path.to.avatars.folder}")
     String path;
 
     public AvatarService(StudentService studentService, AvatarRepo avatarRepo) {
         this.studentService = studentService;
         this.avatarRepo = avatarRepo;
+        this.logger = LoggerFactory.getLogger(AvatarService.class);
     }
 
     public void uploadAvatar(int studentId, MultipartFile avatarFile) throws IOException {
+        currMethod = Thread.currentThread().getStackTrace()[1].getMethodName();
+        logger.debug("Executing method {}", currMethod);
+
         if(avatarFile == null){
             return;
         }
@@ -55,14 +63,23 @@ public class AvatarService {
     }
 
     public Avatar findAvatar(int id) {
+        currMethod = Thread.currentThread().getStackTrace()[1].getMethodName();
+        logger.debug("Executing method {}", currMethod);
+
         return avatarRepo.findByStudentId(id).orElseGet(Avatar::new);
     }
 
     private Object getExtension(String fileName) {
+        currMethod = Thread.currentThread().getStackTrace()[1].getMethodName();
+        logger.debug("Executing method {}", currMethod);
+
         return fileName.substring(fileName.lastIndexOf(".") + 1);
     }
 
     public Page<Avatar> getAll(int offset, int pageSize) {
+        currMethod = Thread.currentThread().getStackTrace()[1].getMethodName();
+        logger.debug("Executing method {}", currMethod);
+
         PageRequest pageRequest = PageRequest.of(offset, pageSize);
         return avatarRepo.findAll(pageRequest);
     }
--- src/main/java/ru/hogwarts/school/service/FacultyService.java
+++ src/main/java/ru/hogwarts/school/service/FacultyService.java
@@ -1,5 +1,7 @@
 package ru.hogwarts.school.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.hogwarts.school.exceptions.NoObjectInRepoException;
@@ -15,38 +17,55 @@ import java.util.stream.Collectors;
 
 @Service
 public class FacultyService {
-final
-FacultyRepo facultyRepo;
+    final
+    private FacultyRepo facultyRepo;
+    private Logger logger;
+    private String currMethod;
 
     public FacultyService(FacultyRepo facultyRepo) {
         this.facultyRepo = facultyRepo;
+        this.logger = LoggerFactory.getLogger(FacultyService.class);
     }
 
     public Faculty addFaculty(Faculty faculty) {
+        currMethod = Thread.currentThread().getStackTrace()[1].getMethodName();
+        logger.debug("Executing method {}", currMethod);
         return facultyRepo.save(faculty);
     }
 
     public Faculty getFaculty(int id) {
+        currMethod = Thread.currentThread().getStackTrace()[1].getMethodName();
+        logger.debug("Executing method {}", currMethod);
         return facultyRepo.findById(id).orElseThrow(() -> new NoObjectInRepoException("No faculty found"));
     }
 
     public void removeFaculty(int id) {
+        currMethod = Thread.currentThread().getStackTrace()[1].getMethodName();
+        logger.debug("Executing method {}", currMethod);
         facultyRepo.deleteById(id);
     }
 
     public Faculty editFaculty(Faculty faculty) {
+        currMethod = Thread.currentThread().getStackTrace()[1].getMethodName();
+        logger.debug("Executing method {}", currMethod);
         return facultyRepo.save(faculty);
     }
 
     public Collection<Faculty> getByColor(String color) {
+        currMethod = Thread.currentThread().getStackTrace()[1].getMethodName();
+        logger.debug("Executing method {}", currMethod);
         return facultyRepo.findByColorContainingIgnoreCase(color);
     }
 
     public Collection<Faculty> getByColorOrName(String color, String name) {
+        currMethod = Thread.currentThread().getStackTrace()[1].getMethodName();
+        logger.debug("Executing method {}", currMethod);
         return facultyRepo.findByColorIgnoreCaseOrNameIgnoreCase(color, name);
     }
 
     public Collection<Student> getStudents(int id) {
-       return getFaculty(id).getStudents();
+        currMethod = Thread.currentThread().getStackTrace()[1].getMethodName();
+        logger.debug("Executing method {}", currMethod);
+        return getFaculty(id).getStudents();
     }
 }
